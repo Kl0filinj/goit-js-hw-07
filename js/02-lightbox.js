0104bc11a20519798a946f3e82a5b2bf77ee919c
Kl0filinj <vladdanya08@gmail.com>
2022-06-28 16:14:34 +0300

    task 2 -in progress

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const makeGalleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', makeGalleryMarkup);
+
+function getCaptionOfImage() {
+  const imageRef = document.querySelector('.gallery__image');
+
+  //   return `${imageRef.alt}`;
+  //   return event.target();
+}
+
+var newGallery = new SimpleLightbox('.gallery a', {
+  captionType: 'text',
+  captionSelector: 'www',
+  captionDelay: 250,
+});
+
+console.log(getCaptionOfImage());
